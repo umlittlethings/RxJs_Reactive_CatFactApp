fb1bea7ad6051a6e5cb99e8bff93a7295658ed9d
kiristoky1 <151371288+kiristoky1@users.noreply.github.com>
2025-05-08 08:17:10 +0700

    edit so it starts with a fact

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-// Use UMD RxJS from global object
 const { fromEvent, of } = rxjs;
 const { switchMap, map, startWith } = rxjs.operators;
 
-// Get DOM elements
 const button = document.getElementById('getFactBtn');
 const output = document.getElementById('catFact');
 
-// Function to fetch cat fact
 const fetchCatFact = () =>
   fetch('https://catfact.ninja/fact').then(res => res.json());
 
-// Reactive event handling
 fromEvent(button, 'click')
   .pipe(
-    startWith(null), // Trigger on page load
+    startWith(null), 
     switchMap(() => fetchCatFact()),
     map(data => data.fact)
   )
